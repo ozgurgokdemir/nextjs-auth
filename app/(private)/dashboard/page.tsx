955da58a309c0e3fa9e1e2fd4365374c2ba4c991
Özgür Gökdemir <ozgurgokd@gmail.com>
2025-03-13 09:24:41 +0300

    Update middleware

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -1,10 +1,4 @@
-import { Button } from '@/components/ui/button';
-import { signOut } from '@/lib/auth/actions';
-import { getUser } from '@/lib/db/queries';
-
 export default async function DashboardPage() {
-  const user = await getUser();
-
   return (
     <main className="flex flex-col items-center sm:items-start min-h-[calc(100dvh-114px)]">
       <div className="border-dashed border-b w-full">
@@ -14,15 +8,7 @@ export default async function DashboardPage() {
           </h1>
         </div>
       </div>
-      <div className="container border-dashed border-x py-12 h-full flex-1">
-        <h2>{user?.name}</h2>
-        <p>{user?.role}</p>
-        <form action={signOut}>
-          <Button className="group w-fit" variant="destructive" type="submit">
-            Sign Out
-          </Button>
-        </form>
-      </div>
+      <div className="container border-dashed border-x py-12 h-full flex-1"></div>
     </main>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,42 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getSession, updateSession } from '@/lib/auth/session';
 
-const protectedRoutes = ['/dashboard'];
-const publicRoutes = ['/signin', '/signup', '/'];
+const ROUTES = {
+  protected: ['/dashboard'],
+  authentication: ['/signin', '/signup'],
+};
+
+const REDIRECTS = {
+  authenticated: '/dashboard',
+  unauthenticated: '/signin',
+};
 
 export default async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  const isProtectedRoute = protectedRoutes.includes(pathname);
-  const isPublicRoute = publicRoutes.includes(pathname);
+  const isProtectedRoute = ROUTES.protected.some((route) =>
+    pathname.startsWith(route)
+  );
+  const isAuthenticationRoute = ROUTES.authentication.some((route) =>
+    pathname.startsWith(route)
+  );
 
   const session = await getSession();
 
   if (!session && isProtectedRoute) {
-    return NextResponse.redirect(new URL('/signin', request.nextUrl));
+    return NextResponse.redirect(
+      new URL(REDIRECTS.unauthenticated, request.nextUrl)
+    );
   }
 
   if (session) {
     await updateSession(session);
   }
 
-  if (session && isPublicRoute && !pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
+  if (session && isAuthenticationRoute) {
+    return NextResponse.redirect(
+      new URL(REDIRECTS.authenticated, request.nextUrl)
+    );
   }
 
   return NextResponse.next();
