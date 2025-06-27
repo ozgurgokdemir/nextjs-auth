f2e008da386ab6f513c6214845ad6f20b3d03069
Özgür Gökdemir <ozgurgokd@gmail.com>
2025-06-27 04:49:52 +0300

    Fix schema regex error

--- lib/auth/definitions.ts
+++ lib/auth/definitions.ts
@@ -25,10 +25,8 @@ export const tokenSchema = z
 
 export const otpSchema = z
   .string()
-  .length(6, { message: 'Code must be 6 characters long' })
-  .regex(new RegExp(REGEXP_ONLY_DIGITS), {
-    message: 'Code must contain only digits',
-  });
+  .length(6, { message: 'Code must be exactly 6 characters long' })
+  .regex(/^\d+$/, { message: 'Code must contain only digits' });
 
 export const signInSchema = z.object({
   email: emailSchema,
